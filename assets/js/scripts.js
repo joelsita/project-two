968ce40c808a3bf75dda4603fce60e9db64a7ccf
Joel Sita <joel.sita@genesys.com>
2024-04-15 20:39:32 +0100

    added an event listen on each game buttons

---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+// declaring variables
+
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+const computerScores = document.getElementById('computerScores');
+const playerScores = document.getElementById('playerScores');
+
+let playerChoice
+let computerChoice
+let result
+let playerScore=0;
+let computerScore=0;
