133530857f61cf535d02f4fe088868e20383803c
Joel Sita <joel.sita@genesys.com>
2024-04-15 20:55:59 +0100

    created a function to get the results of the game and to display

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,15 +25,63 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
 
 function generateComputerChoice() {
   // mapping it to display a whole Number
-  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
   if (randomNumber === 1) {
-    computerChoice = 'rock'
+    computerChoice = 'paper'
   }
   else if (randomNumber === 2) {
     computerChoice = 'scissors'
   }
   else if (randomNumber === 3) {
-    computerChoice = 'paper'
+    computerChoice = 'rock'
   }
-  computerChoiceDisplay.innerHTML = computerChoice
+  // to show much choices comuputer picked on screen
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+// To display results message on screen
+
+function getResult() {
+  if (computerChoice === playerChoice) {
+    result = 'its a draw!, play Again'
+  }
+ else  if  (computerChoice === 'rock' && playerChoice === "paper") {
+    result = 'You Win'
+  }
+  else if (computerChoice === 'rock' && playerChoice === "scissors") {
+    result = 'You Lose'
+  }
+ else if (computerChoice === 'paper' && playerChoice === "scissors") {
+    result = 'You Lose'
+  }
+ else if (computerChoice === 'paper' && playerChoice === "rock") {
+    result = 'You Lose'
+  }
+ else if (computerChoice === 'scissors' && playerChoice === "rock") {
+    result = 'You Win'
+  }
+  else if (computerChoice === 'scissors' && playerChoice === "paper") {
+    result = 'You Lose'
+  }
+  // showing the results on screen with the message
+  resultDisplay.innerHTML = result
+
+// Reseting the game back to zoro to clear the color messages
+  resultDisplay.classList.remove("winText", "loseText");
+  
+  // changing a message color to display different each time comuputer or player win
+  switch(result){
+    case 'You Win':
+      resultDisplay.classList.add("winText")
+      playerScore ++;
+      playerScores.textContent=playerScore;
+      break;
+
+      case 'You Lose':
+        resultDisplay.classList.add("loseText")
+        computerScore ++;
+        computerScores.textContent=computerScore;
+        break;
+  }
+
 }
