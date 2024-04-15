02b446c87c0eadb8e484fd27672903ba512bfad1
Joel Sita <joel.sita@genesys.com>
2024-04-15 20:45:00 +0100

    created a function to display player & computer choices

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-// declaring variables
+//  selecting all the elements and storing the value
 
 const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
@@ -7,8 +7,33 @@ const possibleChoices = document.querySelectorAll('button')
 const computerScores = document.getElementById('computerScores');
 const playerScores = document.getElementById('playerScores');
 
-let playerChoice
-let computerChoice
-let result
+let playerChoice;
+let computerChoice;
+let result;
 let playerScore=0;
 let computerScore=0;
+
+// Adding an event lister to listen to when the game button are clicked
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+  playerChoice = e.target.id
+  userChoiceDisplay.innerHTML = playerChoice;
+  generateComputerChoice();
+  getResult();
+}));
+
+// Generating a random computer choice 
+
+function generateComputerChoice() {
+  // mapping it to display a whole Number
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+  if (randomNumber === 1) {
+    computerChoice = 'rock'
+  }
+  else if (randomNumber === 2) {
+    computerChoice = 'scissors'
+  }
+  else if (randomNumber === 3) {
+    computerChoice = 'paper'
+  }
+  computerChoiceDisplay.innerHTML = computerChoice
+}
